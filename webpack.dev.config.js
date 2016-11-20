7305b523acbe2ca055b35f0e0d11fea227105215
franzmoro <franzmoro88@gmail.com>
2016-11-20 19:52:45 +0000

    :heavy_plus_sign: webpack hot loading
    relates to #2

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,9 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
     './src/index.js'
   ],
   output: {
@@ -14,5 +17,6 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
     ],
-  }
+  },
+  plugins   : [ new webpack.HotModuleReplacementPlugin() ],
 }
