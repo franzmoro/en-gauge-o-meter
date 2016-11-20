680f9892ba24dc429d268940418c60e9bae37432
franzmoro <franzmoro88@gmail.com>
2016-11-20 22:36:14 +0000

    :art: export all on index

--- src/smart_gauge/index.js
+++ src/smart_gauge/index.js
@@ -1,3 +1,6 @@
 module.exports = {
   SmartGaugeContainer: require('./container.js'),
+  SmartGaugeComponent: require('./component.js'),
+  SmartGaugeActions: require('./actions.js'),
+  SmartGaugeReducers: require('./reducers.js'),
 };
