e28970db840052a0601c8188c6e99a9da47f380c
franzmoro <franzmoro88@gmail.com>
2016-11-21 03:31:46 +0000

    :white_check_mark: store creation test
    passing
    
    relates to #4

---
+++ tests/unit/index.test.js
@@ -0,0 +1,22 @@
+require('babel-core/register')({ presets: ['es2015', 'react', 'stage-0'] });
+
+const test = require('tape');
+const nock = require('nock');
+const syncFlow = require('sync-flow');
+const reducers = require('../../src/smart_gauge/reducers.js');
+const configureStore = require('../../src/configure_store.js');
+
+test('UNIT:CONFIG --> CREATE STORE', t => {
+  const { getState } = configureStore({ reducers });
+  const exec = [
+    () => {
+      t.deepEqual(
+        getState().toJS(),
+        require('../fixtures/mock_state.js'),
+        'correct initial state'
+      );
+      t.end();
+    }
+  ];
+  syncFlow(exec,t.end,200);
+});
